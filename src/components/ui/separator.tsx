4444cec027265a356127f8f54e72d1270ec591ef
Trendyzima <momanyitrendy@gmail.com>
2026-09-06 08:50:14 +0300

    fix: restore missing separator primitive

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+export const Separator=React.forwardRef<HTMLDivElement,React.HTMLAttributes<HTMLDivElement> & {orientation?:'horizontal'|'vertical'}>(({className,orientation='horizontal',...props},ref)=><div ref={ref} role="separator" className={cn('shrink-0 bg-border',orientation==='vertical'?'h-full w-px':'h-px w-full',className)} {...props}/>);
+Separator.displayName='Separator';
