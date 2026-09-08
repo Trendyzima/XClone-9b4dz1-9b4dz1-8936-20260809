e469a1859ef70fd7e23e6a4443f4514dfb79b69f
Trendyzima <momanyitrendy@gmail.com>
2026-09-08 17:03:46 +0300

    security: pin federation trigger function search paths

---
+++ supabase/migrations/20260908140500_fix_federation_function_search_paths.sql
@@ -0,0 +1,7 @@
+-- Security hardening: pin trigger-function search_path to prevent
+-- session-controlled name resolution.
+alter function public.federation_actor_workersdev_canonical()
+  set search_path = '';
+
+alter function public.federation_canonicalize_actor_url()
+  set search_path = '';
