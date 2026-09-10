178d4aba053bf211f01777a50aac83dbd87e7044
Trendyzima <momanyitrendy@gmail.com>
2026-09-10 10:40:15 +0300

    Align migration source version with production history

--- supabase/migrations/20260910073600_repair_federated_interaction_projection_integrity.sql
+++
@@ -1,5 +0,0 @@
-alter table public.federated_object_interactions drop constraint if exists federated_object_interactions_object_uri_fkey;
-alter table public.federated_object_interactions drop constraint if exists federated_object_interactions_interaction_type_check;
-alter table public.federated_object_interactions add constraint federated_object_interactions_interaction_type_check check (interaction_type = any (array['like','announce','repost','reply','quote','bookmark','view','not_interested']));
-create index if not exists federated_interactions_activity_idx on public.federated_object_interactions(activity_uri) where activity_uri is not null;
-notify pgrst,'reload schema';
